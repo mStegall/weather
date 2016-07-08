6dfc03212f24b75e2c93ffe3e9799b3d37bd4e27
Matt Stegall <mstegall928@gmail.com>
2016-07-07 21:48:22 -0400

    Incorporate Bower Install into Gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,34 +3,39 @@ var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
+var bower = require('gulp-bower');
+
+gulp.task('bower', function () {
+    return bower();
+})
 
 gulp.task('pug', function () {
     return gulp.src('./src/**/*.pug')
-            .pipe(pug())
-            .pipe(gulp.dest('./public/'))
+        .pipe(pug())
+        .pipe(gulp.dest('./public/'))
 })
 
 gulp.task('sass', function () {
     return gulp.src('./src/**/*.scss')
-            .pipe(sass({
-                includePaths: ['./src/assets/lib/bootstrap-sass/assets/stylesheets', './src/assets/lib/bootswatch/flatly']
-            }))
-            .pipe(gulp.dest('./public/'))
+        .pipe(sass({
+            includePaths: ['./src/assets/lib/bootstrap-sass/assets/stylesheets', './src/assets/lib/bootswatch/flatly']
+        }))
+        .pipe(gulp.dest('./public/'))
 })
 
 gulp.task('assets', function () {
-    return gulp.src('./src/assets/*')
-            .pipe(gulp.dest('./public/assets/'))
+    return gulp.src('./src/assets/**/*')
+        .pipe(gulp.dest('./public/assets/'))
 })
 
 gulp.task('clean', function () {
-    return gulp.src('./public/', {read: false})
-            .pipe(clean()); 
+    return gulp.src('./public/', { read: false })
+        .pipe(clean());
 })
 
 gulp.task('build', function () {
-    runSequence('clean',
-                ['pug','sass','assets'])
+    runSequence(['clean', 'bower'],
+        ['pug', 'sass', 'assets'])
 })
 
 gulp.task('default', ['build'])
